14145a90eb1348586891ae67d396e4717ceb9a17
Daniel Collin <daniel@collin.com>
2014-05-22 08:58:24 +0200

    Updated as scRunKernel1DArray(...) has changed

--- examples/add_floats/add_floats.c
+++ examples/add_floats/add_floats.c
@@ -1,6 +1,5 @@
 #include <sico.h>
 #include <stdio.h>
-#include <malloc.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -8,10 +7,11 @@ int main(void)
 {
     size_t i;
 	float inputData[] = { 1.0f, 2.0f, 3.0f, 4.0f };
+	float inputData2[] = { 11.0f, 12.0f, 13.0f, 14.0f };
 	const size_t inputCount = oclw_sizeof_array(inputData);
 	float dataRes[oclw_sizeof_array(inputData)];
 
-    scRunKernel1DArray(dataRes, inputData, "examples/add_floats/add_floats.cl", inputCount, sizeof(inputData));
+    scRunKernel1DArray(dataRes, inputData, inputData2, "examples/add_floats/add_floats.cl", inputCount, sizeof(inputData));
 
     for (i = 0; i < inputCount; ++i)
         printf("data %f\n", dataRes[i]);
--- examples/add_floats/add_floats.cl
+++ examples/add_floats/add_floats.cl
@@ -1,7 +1,7 @@
 // Basic example of adding floats from one input and write to another output
 
-__kernel void kern(global float* output, global float* input)
+__kernel void kern(global float* output, global float* inputA, global float* inputB)
 {
     size_t i = get_global_id(0);
-    output[i] = input[i] + input[i];
+    output[i] = inputA[i] + inputB[i];
 }
